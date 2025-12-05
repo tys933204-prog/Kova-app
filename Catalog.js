7ac52e595cb38a7ddbe7589eacc0f3275f63ee93
tys933204-prog <tys933204@gmail.com>
2025-12-04 23:20:14 -0600

    Update Catalog.js

--- Catalog.js
+++ Catalog.js
@@ -1,1 +1,20 @@
+// Catalog.js
+// The main browsing section for all fashion products
 
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function Catalog() {
+  return (
+    <View style={{ flex: 1, backgroundColor: "#111", padding: 20 }}>
+      <Text style={{ color: "#0f0", fontSize: 26, fontWeight: "600" }}>
+        Catalog
+      </Text>
+
+      <Text style={{ color: "#ccc", marginTop: 10 }}>
+        Here users will browse ALL products from:
+        Amazon, SHEIN, H&M, Uniqlo, ASOS, Walmart, Nordstrom, and more.
+      </Text>
+    </View>
+  );
+}
